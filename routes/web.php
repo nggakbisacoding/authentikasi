97f3efe5c43f118aa570b96eee9e2231f41410f3
nggakbisacoding <fajarwahyunugroho0802@mail.ugm.ac.id>
2023-12-05 12:30:26 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,22 @@ use App\Http\Controllers\SendEmailController;
 |
 */
 
-Route::get('/', function () {
+Route::get('/public/', function () {
     return view('welcome');
 });
 
-Route::get('/send-mail', [SendEmailController::class, 'index'])->name('kirim-email');
-Route::post('/post-email', [SendEmailController::class, 'store'])->name('post-email');
+Route::get('/public/send-mail', [SendEmailController::class, 'index'])->name('kirim-email');
+Route::post('/public/post-email', [SendEmailController::class, 'store'])->name('post-email');
 
 Route::controller(LoginRegisterController::class)->group(function() {
-    Route::get('/register', 'register')->name('register');
-    Route::post('/store', 'store')->name('store');
-    Route::get('/login', 'login')->name('login');
-    Route::post('/authenticate', 'authenticate')->name('authenticate');
-    Route::get('/dashboard', 'dashboard')->name('dashboard');
-    Route::post('/logout', 'logout')->name('logout');
-    Route::get('/edit/{id}', 'edit')->name('edit');
-    Route::put('/edit/{id}', 'update')->name('update');
+    Route::get('/public/register', 'register')->name('register');
+    Route::post('/public/store', 'store')->name('store');
+    Route::get('/public/login', 'login')->name('login');
+    Route::post('/public/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/public/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/public/logout', 'logout')->name('logout');
+    Route::get('/public/edit/{id}', 'edit')->name('edit');
+    Route::put('/public/edit/{id}', 'update')->name('update');
 });
 
-Route::get('/greet', [GreetController::class, 'greet'])->name('greet');
+Route::get('/public/greet', [GreetController::class, 'greet'])->name('greet');
